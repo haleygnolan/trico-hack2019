97a0c88e6331219f6c5efa7c446143805a9aecec
Sonya Fucci <sfucci@brynmawr.edu>
2019-02-09 13:35:14 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 import java.io.*;
+import java.time.format.TextStyle;
 import java.time.LocalTime;
-import java.util.Arrays;
+import java.time.LocalDate;
+import java.time.DayOfWeek;
+import java.util.Locale;
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Scanner;
 
@@ -31,13 +35,13 @@ public class Main{
     meals.put("d","dinner");
     meals.put("br","brunch/lunch");
 
-    daysInt.put("m",0);
-    daysInt.put("t",1);
-    daysInt.put("w",2);
-    daysInt.put("r",3);
-    daysInt.put("f",4);
-    daysInt.put("s",5);
-    daysInt.put("u",6);
+    daysInt.put("Monday",0);
+    daysInt.put("Tuesday",1);
+    daysInt.put("Wednesday",2);
+    daysInt.put("Thursday",3);
+    daysInt.put("Friday",4);
+    daysInt.put("Saturday",5);
+    daysInt.put("Sunday",6);
 
 
     daysToString.put(0,"Monday");
@@ -49,13 +53,21 @@ public class Main{
     daysToString.put(6,"Sunday");
 
     Scanner scanner = new Scanner(new File(MENU_CSV));
+    ArrayList<Hours> hours = new ArrayList<Hours>();
     while(scanner.hasNextLine()){
       String str = scanner.nextLine();
       String[] info = str.split(",");
       Hours hour = new Hours(info[HALL],info[DATE],info[MEAL],
       info[OPEN_HOUR],info[CLOSE_HOUR], halls, meals, daysInt, daysToString);
-      System.out.println(hour.toString());
+
+      hours.add(hour);
+
     }
+
+    for(int i = 0; i<hours.size(); i++){
+      System.out.println(hours.get(i).toString() + "   "+ hours.get(i).isOpenNow());
+    }
+
   }
 
 
@@ -88,16 +100,36 @@ class Hours{
     String[] endSplit = end.split(":");
     this.end = LocalTime.of(Integer.parseInt(endSplit[0]),Integer.parseInt(endSplit[1]));
 
-    days[daysInt.get("m")] = range.indexOf("m") != -1;
-    days[daysInt.get("t")] = range.indexOf("t") != -1;
-    days[daysInt.get("w")] = range.indexOf("w") != -1;
-    days[daysInt.get("r")] = range.indexOf("r") != -1;
-    days[daysInt.get("f")] = range.indexOf("f") != -1;
-    days[daysInt.get("s")] = range.indexOf("s") != -1;
-    days[daysInt.get("u")] = range.indexOf("u") != -1;
+    days[daysInt.get("Monday")] = range.indexOf("m") != -1;
+    days[daysInt.get("Tuesday")] = range.indexOf("t") != -1;
+    days[daysInt.get("Wednesday")] = range.indexOf("w") != -1;
+    days[daysInt.get("Thursday")] = range.indexOf("r") != -1;
+    days[daysInt.get("Friday")] = range.indexOf("f") != -1;
+    days[daysInt.get("Saturday")] = range.indexOf("s") != -1;
+    days[daysInt.get("Sunday")] = range.indexOf("u") != -1;
   }
 
-  String getDays(){
+  public String getHall(){
+    return hall;
+  }
+
+  public Boolean isOpenDay(String dayOfWeek){
+    return days[daysInt.get(dayOfWeek)];
+  }
+
+  public Boolean isAfterStartTime(){
+    return start.isBefore(LocalTime.now());
+  }
+
+  public Boolean isBeforeEndTime(){
+    return end.isAfter(LocalTime.now());
+  }
+
+  public Boolean isOpenNow(){
+    return isBeforeEndTime() && isAfterStartTime() && isOpenDay(LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.FULL,new Locale("en")));
+  }
+
+  public String getDays(){
     String str = "";
     for(int i = 0; i<days.length;i++){
       if(days[i]){
